1e9e570f1280563f42c29718482a290980ee2f04
Keith Crooc <keith.crooc@gmail.com>
2021-04-24 18:49:08 -0400

    Project 6b where we play with constraints and auto layout in the viewController

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -12,8 +12,80 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        let metrics = ["labelHeight": 88]
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+            
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            previous = label
+        }
     }
 
 
 }
 
+
+// CHALLENGE
+// 1. replace widthAnchor leadingAnchor and trailingAnchor
+        // - widthAnchor would flex display it, taking up 100%. leadingAnchor pins it left, trailingAnchor to the right.
+
+// 2. use "safeAreaLayoutGuide" with these ^^^ constraints (will see if working right when you put it in landscape mode)
+        // this makes sure the notch doesn't get in the way when in landscape.
+
+
+// 3. Make the height of labels = 1/5th of the main view, minus 10 for the spacing between.
+        //  heightAnchor constraints, we pass in 3 parameters: equalTo, multiplier, constant.
+        //  equalTo sets initial measurement (in this case it's the size of the screen),
+        //  multiplier, we want to fit our 5 labels in. We will set this 1/5th of the view, so we put 0.2 to represent that (20%)
+        //  constant: we set this to -10 to account for the space between.
